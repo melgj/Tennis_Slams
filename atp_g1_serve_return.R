0bb52efedd21ab7df13361ca9741dfeafc6dd31a
melgj <cry-havoc22@hushmail.com>
2021-02-11 00:23:30 +0000

    first commit

---
+++ atp_g1_serve_return.R
@@ -0,0 +1,174 @@
+library(tidyverse)
+library(lubridate)
+library(stringr)
+
+atpPts <- read_csv("atp_slam_points_all.csv", col_names = T)
+
+str(atpPts)
+
+t50 <- read_csv("atp_SR_ratings_jan21.csv", col_names = T) %>% 
+  mutate(Rank = min_rank(desc(Rating))) %>% 
+  filter(Rank <= 50) %>% 
+  select(Rank, everything())
+
+tail(t50)
+
+g1 <- atpPts %>% 
+  filter(SetNo == 1, GameNo == 1, GameWinner != 0,
+         (player1 %in% t50$Player | player2 %in% t50$Player)) %>% 
+  select(match_id:PointServer, P1Score, P2Score, 
+         player1, player2, player1id, player2id)
+
+#####################################
+
+# SERVE
+
+g1s <- g1 %>% 
+  mutate(Server_Name = if_else(PointServer == 1, player1, player2),
+         Winner_Name = if_else(GameWinner == 1, player1, player2),
+         Held_Serve = if_else(GameWinner == PointServer, T, F))
+
+g1s_summary <- g1s %>% 
+  group_by(Server_Name) %>% 
+  summarise(Service_Games_G1 = n(),
+            Held_G1 = sum(Held_Serve == T),
+            Broken_G1 = sum(Held_Serve == F)) %>% 
+  mutate(Held_Pct_G1 = signif(Held_G1/(Held_G1+Broken_G1), 2)) %>% 
+  #filter(Service_Games_G1 >= 10) %>% 
+  arrange(desc(Held_Pct_G1))
+
+g1s_summary %>% 
+  filter(Service_Games_G1 >= 10) %>% 
+  arrange(desc(Held_Pct_G1))
+  
+
+###########################################
+
+allGms <- atpPts %>% 
+  filter(GameWinner != 0, (player1 %in% t50$Player | player2 %in% t50$Player)) %>% 
+  select(match_id:PointServer, P1Score, P2Score, 
+         player1, player2, player1id, player2id) 
+
+head(allGms)
+
+allGmsS <- allGms %>% 
+  mutate(Server_Name = if_else(PointServer == 1, player1, player2),
+         Winner_Name = if_else(GameWinner == 1, player1, player2),
+         Held_Serve = if_else(GameWinner == PointServer, T, F))
+
+allGmsS_summary <- allGmsS %>% 
+  group_by(Server_Name) %>% 
+  summarise(Service_Games_All = n(),
+            Held_All = sum(Held_Serve == T),
+            Broken_All = sum(Held_Serve == F)) %>% 
+  mutate(Held_Pct_All = signif(Held_All/(Held_All + Broken_All),2)) %>% 
+  #filter(Service_Games_All >= 50) %>% 
+  arrange(desc(Held_Pct_All))
+
+allGmsS_summary %>% 
+  filter(Service_Games_All >= 100) %>% 
+  arrange(desc(Held_Pct_All))
+
+comb_server <- allGmsS_summary %>% 
+  left_join(g1s_summary, by = "Server_Name") %>% 
+  mutate(Abs_Diff_Serve_G1vAll = abs(Held_Pct_G1 - Held_Pct_All),
+         Net_Diff_Serve_G1vAll = Held_Pct_G1 - Held_Pct_All) %>% 
+  select(Server_Name, Service_Games_All, Held_Pct_All, Service_Games_G1, 
+         Held_Pct_G1, Abs_Diff_Serve_G1vAll, Net_Diff_Serve_G1vAll) %>% 
+  arrange(desc(Net_Diff_Serve_G1vAll))
+
+comb_server %>% 
+  filter(Service_Games_All >= 100) %>%
+  drop_na() %>% 
+  arrange(desc(Net_Diff_Serve_G1vAll))
+
+tail(comb_server)
+
+##############################################
+
+# RETURN
+
+g1r <- g1 %>% 
+  mutate(Receiver_Name = if_else(PointServer == 1, player2, player1),
+         Winner_Name = if_else(GameWinner == 1, player1, player2),
+         Broke_Serve = if_else(GameWinner != PointServer, T, F))
+
+g1r_summary <- g1r %>% 
+  group_by(Receiver_Name) %>% 
+  summarise(Receiver_Games_G1 = n(),
+            Broke_G1 = sum(Broke_Serve == T),
+            Lost_G1 = sum(Broke_Serve == F)) %>% 
+  mutate(Broke_Pct_G1 = signif(Broke_G1/(Broke_G1 + Lost_G1), 2)) %>% 
+  #filter(Receiver_Games_G1 >= 10) %>% 
+  arrange(desc(Broke_Pct_G1))
+
+g1r_summary %>% 
+  filter(Receiver_Games_G1 >= 10) %>% 
+  arrange(desc(Broke_Pct_G1))
+
+###########################################
+
+allGmsR <- allGms %>% 
+  mutate(Receiver_Name = if_else(PointServer == 1, player2, player1),
+         Winner_Name = if_else(GameWinner == 1, player1, player2),
+         Broke_Serve = if_else(GameWinner != PointServer, T, F))
+
+allGmsR_summary <- allGmsR %>% 
+  group_by(Receiver_Name) %>% 
+  summarise(Receiver_Games_All = n(),
+            Broke_All = sum(Broke_Serve == T),
+            Lost_All = sum(Broke_Serve == F)) %>% 
+  mutate(Broke_Pct_All = signif(Broke_All/(Broke_All + Lost_All), 2)) %>% 
+  #filter(Receiver_Games_All >= 50) %>% 
+  arrange(desc(Broke_Pct_All))
+
+allGmsR_summary %>% 
+  filter(Receiver_Games_All >= 50) %>% 
+  arrange(desc(Broke_Pct_All))
+
+comb_receiver <- allGmsR_summary %>% 
+  left_join(g1r_summary, by = "Receiver_Name") %>% 
+  mutate(Abs_Diff_Rec_G1vAll = abs(Broke_Pct_G1 - Broke_Pct_All),
+         Net_Diff_Rec_G1vAll = Broke_Pct_G1 - Broke_Pct_All) %>% 
+  select(Receiver_Name, Receiver_Games_All, Broke_Pct_All, Receiver_Games_G1, 
+         Broke_Pct_G1, Abs_Diff_Rec_G1vAll, Net_Diff_Rec_G1vAll) %>% 
+  arrange(desc(Net_Diff_Rec_G1vAll)) 
+
+comb_receiver %>% 
+  filter(Receiver_Games_All >= 50) %>%
+  drop_na() %>% 
+  arrange(desc(Net_Diff_Rec_G1vAll))
+
+tail(comb_receiver)
+
+
+#######################################
+
+comb_all <- comb_server %>% 
+  full_join(comb_receiver, by = c("Server_Name" = "Receiver_Name")) %>% 
+  rename(Player = Server_Name)
+
+colSums(is.na(comb_all))
+x <- which(is.na(comb_all$Receiver_Games_G1))
+
+comb_all[x,]
+
+comb_all_summary <- comb_all %>% 
+  mutate(Game1_Win_Pct_Diff = Held_Pct_G1 - Broke_Pct_G1,
+         Matches = Service_Games_G1 + Receiver_Games_G1,
+         Served_1st_Pct = signif(Service_Games_G1 / Matches, 2)) %>% 
+  filter(Matches >= 25) %>% 
+  select(Player, Matches, Service_Games_G1, Receiver_Games_G1, Served_1st_Pct,
+         Game1_Win_Pct_Diff, Held_Pct_G1, Broke_Pct_G1) %>% 
+  arrange(desc(Game1_Win_Pct_Diff)) 
+
+comb_all_summary %>% 
+  drop_na() %>% 
+  arrange(desc(Game1_Win_Pct_Diff)) 
+
+view(comb_all_summary)
+
+tail(comb_all_summary)
+
+mean(comb_all_summary$Held_Pct_G1)
+mean(comb_all_summary$Broke_Pct_G1)
---
+++ slam_all_courts_pbp.R
@@ -0,0 +1,59 @@
+library(tidyverse)
+library(lubridate)
+
+allPoints <- list.files(pattern="*points.csv") %>%
+  map_df(~read_csv(., col_types = cols(.default = "c")))
+
+colnames(allPoints)
+
+allMatches <- list.files(pattern="*matches.csv") %>%
+  map_df(~read_csv(., col_types = cols(.default = "c")))
+
+colnames(allMatches)
+
+w <- read_csv("/home/magljo/wta/wta_tennis/SR_ratings_jan21.csv", col_names = T)
+m <- read_csv("/home/magljo/atp/atp_SR_ratings_jan21.csv", col_names = T)
+
+w50 <- w %>% 
+  mutate(Rank = min_rank(desc(Rating))) %>% 
+  select(Rank, everything()) %>% 
+  filter(Rank <= 50)
+
+w50
+
+m50 <- m %>% 
+  mutate(Rank = min_rank(desc(Rating))) %>% 
+  select(Rank, everything()) %>% 
+  filter(Rank <= 50)
+
+m50
+
+  
+# Write data to csv file
+
+write_csv(allPoints, "slam_points_all.csv")
+write_csv(allMatches, "slam_matches_all.csv")
+
+###########
+
+# pts <- read_csv("slams_points_hard.csv", col_names = T)
+# mts <- read_csv("slam_matches_hard.csv", col_names = T)
+
+sp <- allPoints %>% 
+  left_join(allMatches, by = "match_id")
+
+head(sp)
+
+wta <- sp %>% 
+  filter(player1 %in% w50$Player | player2 %in% w50$Player)
+
+atp <- sp %>% 
+  filter(player1 %in% m50$Player | player2 %in% m50$Player)
+
+write_csv(wta, "wta_slam_points_all.csv")
+write_csv(atp, "atp_slam_points_all.csv")
+
+view(head(wta))
+
+colnames(wta)
+
