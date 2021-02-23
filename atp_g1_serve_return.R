660993f880fcd701efc21021ffb486364d858e2b
melgj <cry-havoc22@hushmail.com>
2021-02-23 10:19:59 +0000

    add serve return stats/plots by surface

--- atp_g1_serve_return.R
+++ atp_g1_serve_return.R
@@ -4,7 +4,11 @@ library(stringr)
 
 atpPts <- read_csv("atp_slam_points_all.csv", col_names = T)
 
-str(atpPts)
+glimpse(atpPts)
+
+atpPts$Surface <- case_when(atpPts$slam == "ausopen" | atpPts$slam == "usopen" ~ "Hard",
+                            atpPts$slam == "frenchopen" ~ "Clay",
+                            atpPts$slam == "wimbledon" ~ "Grass")
 
 t50 <- read_csv("atp_SR_ratings_jan21.csv", col_names = T) %>% 
   mutate(Rank = min_rank(desc(Rating))) %>% 
@@ -17,7 +21,7 @@ g1 <- atpPts %>%
   filter(SetNo == 1, GameNo == 1, GameWinner != 0,
          (player1 %in% t50$Player | player2 %in% t50$Player)) %>% 
   select(match_id:PointServer, P1Score, P2Score, 
-         player1, player2, player1id, player2id)
+         player1, player2, player1id, player2id, Surface)
 
 #####################################
 
@@ -47,7 +51,7 @@ g1s_summary %>%
 allGms <- atpPts %>% 
   filter(GameWinner != 0, (player1 %in% t50$Player | player2 %in% t50$Player)) %>% 
   select(match_id:PointServer, P1Score, P2Score, 
-         player1, player2, player1id, player2id) 
+         player1, player2, player1id, player2id, Surface) 
 
 head(allGms)
 
@@ -123,8 +127,10 @@ allGmsR_summary <- allGmsR %>%
   arrange(desc(Broke_Pct_All))
 
 allGmsR_summary %>% 
-  filter(Receiver_Games_All >= 50) %>% 
+  filter(Receiver_Games_All >= 100) %>% 
   arrange(desc(Broke_Pct_All))
+
+allGmsR_summary
 
 comb_receiver <- allGmsR_summary %>% 
   left_join(g1r_summary, by = "Receiver_Name") %>% 
@@ -166,9 +172,86 @@ comb_all_summary %>%
   drop_na() %>% 
   arrange(desc(Game1_Win_Pct_Diff)) 
 
-view(comb_all_summary)
+#view(comb_all_summary)
 
 tail(comb_all_summary)
 
 mean(comb_all_summary$Held_Pct_G1)
 mean(comb_all_summary$Broke_Pct_G1)
+
+# Summary Stats and Plots
+
+# By Surface
+allGmsS %>% 
+  group_by(Server_Name, Surface) %>% 
+  summarise(Service_Games_All = n(),
+            Held_All = sum(Held_Serve == T),
+            Broken_All = sum(Held_Serve == F)) %>% 
+  mutate(Held_Pct_All = signif(Held_All/(Held_All + Broken_All),2)) %>% 
+  filter(Service_Games_All >= 50, Held_Pct_All >= 0.85) %>% 
+  arrange(Surface, desc(Held_Pct_All)) %>% 
+  ggplot(aes(reorder(Server_Name, Held_Pct_All), Held_Pct_All, fill = Service_Games_All)) +
+  geom_col() +
+  geom_text(aes(label = Held_Pct_All), nudge_y = -0.2, colour = "orange") +
+  labs(title = "Best Service Hold Percentage by Surface in Grand Slams (Men)", subtitle = "minimum 50 service games") +
+  ylab("Hold Percentage") +
+  xlab("Player") +
+  theme(plot.title = element_text(hjust = 0.5))+
+  coord_flip() +
+  facet_wrap(~ Surface)
+
+allGmsR %>% 
+  group_by(Receiver_Name, Surface) %>% 
+  summarise(Receiver_Games_All = n(),
+            Broke_All = sum(Broke_Serve == T),
+            Lost_All = sum(Broke_Serve == F)) %>% 
+  mutate(Broke_Pct_All = signif(Broke_All/(Broke_All + Lost_All), 2)) %>% 
+  filter(Receiver_Games_All >= 50, Broke_Pct_All >= 0.25) %>% 
+  arrange(Surface, desc(Broke_Pct_All)) %>% 
+  ggplot(aes(reorder(Receiver_Name, Broke_Pct_All), Broke_Pct_All, fill = Receiver_Games_All)) +
+  geom_col() +
+  geom_text(aes(label = Broke_Pct_All), nudge_y = -0.1, colour = "orange") +
+  labs(title = "Best Service Break Percentage by Surface in Grand Slams (Men)", subtitle = "minimum 50 return games") +
+  ylab("Break Percentage") +
+  xlab("Player") +
+  theme(plot.title = element_text(hjust = 0.5))+
+  coord_flip() +
+  facet_wrap(~ Surface)
+
+# All Surfaces
+
+allGmsS %>% 
+  group_by(Server_Name) %>% 
+  summarise(Service_Games_All = n(),
+            Held_All = sum(Held_Serve == T),
+            Broken_All = sum(Held_Serve == F)) %>% 
+  mutate(Held_Pct_All = signif(Held_All/(Held_All + Broken_All),2)) %>% 
+  filter(Service_Games_All >= 50, Held_Pct_All >= 0.85) %>% 
+  arrange(desc(Held_Pct_All)) %>% 
+  ggplot(aes(reorder(Server_Name, Held_Pct_All), Held_Pct_All, fill = Service_Games_All)) +
+  geom_col() +
+  geom_text(aes(label = Held_Pct_All), nudge_y = -0.2, colour = "orange") +
+  labs(title = "Best Service Hold Percentage in Grand Slams (Men)", subtitle = "minimum 50 service games") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  ylab("Hold Percentage") +
+  xlab("Player") +
+  coord_flip()
+   
+
+allGmsR %>% 
+  group_by(Receiver_Name) %>% 
+  summarise(Receiver_Games_All = n(),
+            Broke_All = sum(Broke_Serve == T),
+            Lost_All = sum(Broke_Serve == F)) %>% 
+  mutate(Broke_Pct_All = signif(Broke_All/(Broke_All + Lost_All), 2)) %>% 
+  filter(Receiver_Games_All >= 50, Broke_Pct_All >= 0.25) %>% 
+  arrange(desc(Broke_Pct_All))%>% 
+  ggplot(aes(reorder(Receiver_Name, Broke_Pct_All), Broke_Pct_All, fill = Receiver_Games_All)) +
+  geom_col() +
+  geom_text(aes(label = Broke_Pct_All), nudge_y = -0.1, colour = "orange") +
+  labs(title = "Best Service Break Percentage in Grand Slams (Men)", subtitle = "minimum 50 return games") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  ylab("Break Percentage") +
+  xlab("Player") +
+  coord_flip()
+
